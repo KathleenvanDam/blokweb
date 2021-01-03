31f46dd3f536c2f842335b17756684ea4fb3de24
KathleenvanDam <74105581+KathleenvanDam@users.noreply.github.com>
2021-01-03 16:05:42 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+var button = document.querySelector("#hamburger");
+var navigatie = document.querySelector("#subNav");
+
+button.addEventListener("click", uitschuiven);
+
+function uitschuiven(){
+    navigatie.classList.toggle('active')
+}
+
+
+/*var button = document.querySelector("button");
+var navigatie = document.querySelector("nav");
+
+navigatie.classList.add("subNav");
+
+button.addEventListener("click"), function(){
+    navigatie.classList.toggle("subNav");
+}*/
